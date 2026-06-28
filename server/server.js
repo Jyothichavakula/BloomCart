6bffb1779bfc74dd54c1fdf7ef98037b8d9dc637
Jyothichavakula <jyothichavakula15@gmail.com>
2026-06-28 19:27:10 +0530

    Updated server configuration for deployment

--- server/server.js
+++ server/server.js
@@ -72,17 +72,17 @@ app.use(
 );
 
 // DATABASE CONNECTION
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
 
 .then(() => {
 
-    console.log('MongoDB Connected');
+    console.log("MongoDB Connected");
 
-    app.listen(process.env.PORT, () => {
+    app.listen(PORT, () => {
 
-        console.log(
-            `Server running on port ${process.env.PORT}`
-        );
+        console.log(`Server running on port ${PORT}`);
 
     });
 
